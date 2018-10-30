214c6a29b37c0440cb7ecb93a342e70cf60b5ece
Vasilkov Dmitry <riset24@mail.ru>
2018-10-30 14:18:02 +0300

    delete all letters

--- src/app/email/letters/letters.component.ts
+++ src/app/email/letters/letters.component.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, ElementRef, OnInit} from '@angular/core';
+import { Component, DoCheck, ElementRef, OnInit } from '@angular/core';
 import { EmailServiceService } from '../email-service.service';
 import { Router, Scroll } from '@angular/router';
 import { TouchSequence } from 'selenium-webdriver';
@@ -17,12 +17,9 @@ export class LettersComponent implements DoCheck, OnInit {
   openLetter = true;
   visibleMenu = true;
   counterAmount = 0;
-  counterFroDownload = 0;
+  counterFromDownload = 0;
   stopFlag = false;
   dataLetters;
-
-
-
 
   constructor(
     public emailServ: EmailServiceService,
@@ -30,19 +27,19 @@ export class LettersComponent implements DoCheck, OnInit {
     private rout: Router
   ) {}
 
-
   ngOnInit() {
     this.emailServ.dataLetters = this.emailServ.lettersAmount;
   }
 
   ngDoCheck() {
+    console.log(this.emailServ.lettersList)
   }
 
-
   activeEl(param, id) {
-
     // tslint:disable-next-line:max-line-length
-    this.emailServ.httpPost('http://10.0.1.33:3000/mail/seen', {id : id, flag: true}).subscribe();  // перевожу в прочитанные сообщения
+    this.emailServ
+      .httpPost('http://10.0.1.33:3000/mail/seen', { id: id, flag: true })
+      .subscribe(); // перевожу в прочитанные сообщения
     this.emailServ.lettersList[param].seen = true;
     // tslint:disable-next-line:forin
     for (const i in this.emailServ.activeLett) {
@@ -51,17 +48,16 @@ export class LettersComponent implements DoCheck, OnInit {
     this.emailServ.activeLett[param] = !this.emailServ.activeLett[param];
 
     this.emailServ.mailsToArray = []; // очистил список отправителей
-    this.emailServ.mailsToArray.push(this.emailServ.lettersList[param].mail_from);  // добавил в список отправителей
+    this.emailServ.mailsToArray.push(
+      this.emailServ.lettersList[param].mail_from
+    ); // добавил в список отправителей
     this.emailServ.subjectTo = this.emailServ.lettersList[param].subject;
   }
 
   hideAva(index) {
     this.emailServ.hideAvatars[index] = !this.emailServ.hideAvatars[index];
   }
-  urlLetterView(
-    idLetter,
-    currentId,
-  ) {
+  urlLetterView(idLetter, currentId) {
     this.rout.navigate([this.emailServ.urlParams + '/view' + '/' + idLetter]);
     this.emailServ.selectedLetter = this.emailServ.lettersList[idLetter];
     this.emailServ.index = idLetter;
@@ -69,15 +65,16 @@ export class LettersComponent implements DoCheck, OnInit {
     this.emailServ.hiddenEmpty = true;
 
     this.emailServ.fullPath =
-    this.emailServ.urlParams + '/view' + '/' + idLetter;
+      this.emailServ.urlParams + '/view' + '/' + idLetter;
     this.emailServ.currentId = currentId; // test
 
     this.emailServ.stateServ();
   }
 
-  selectedLetters(id, e, i) {   // множественный выбор писем в папке ****************
+  selectedLetters(id, e, i) {
+    // множественный выбор писем в папке ****************
     if (e.target.checked) {
-      this.emailServ.idLetters = [...this.emailServ.idLetters, id];  // индексы писем (от 0 до ...)
+      this.emailServ.idLetters = [...this.emailServ.idLetters, id]; // индексы писем (от 0 до ...)
       this.emailServ.idLetters = this.emailServ.idLetters.filter(
         (val, ind, self) => {
           return self.indexOf(val) === ind;
@@ -118,7 +115,7 @@ export class LettersComponent implements DoCheck, OnInit {
       hiddenBlock.classList.toggle('visible');
     }
   }
-// ****************************************spam add - delete***************************** */
+  // ****************************************spam add - delete***************************** */
   spamMark(i, e, booleanParam, id) {
     // if (this.emailServ.lettersList[i].messageCondition !== undefined) {
     this.emailServ.lettersList[i].box = booleanParam;
@@ -130,126 +127,162 @@ export class LettersComponent implements DoCheck, OnInit {
 
     setTimeout(() => {
       if (booleanParam === 4) {
-      this.emailServ.lettersList = this.emailServ.lettersList.filter((val , ind) => {
-        if (val.box !== 4) {
-          return val;
-        }
-        });
+        this.emailServ.lettersList = this.emailServ.lettersList.filter(
+          (val, ind) => {
+            if (val.box !== 4) {
+              return val;
+            }
+          }
+        );
       }
       if (booleanParam === 0) {
-        this.emailServ.lettersList = this.emailServ.lettersList.filter((val , ind) => {
-          if (val.box !== 0) {
-            return val;
+        this.emailServ.lettersList = this.emailServ.lettersList.filter(
+          (val, ind) => {
+            if (val.box !== 0) {
+              return val;
+            }
           }
-          });
-        }
-        this.emailServ.httpPost('http://10.0.1.33:3000/mail/setbox', {id : id, box: `${booleanParam}`}).subscribe();
+        );
+      }
+      this.emailServ
+        .httpPost('http://10.0.1.33:3000/mail/setbox', {
+          id: id,
+          box: `${booleanParam}`
+        })
+        .subscribe();
     }, 500);
-
   }
-
 
   // *****************************************************************************
 
-  toggleImportantMark(i, e, id, boolean) {  // для переключения удалить-добавить важное
+  toggleImportantMark(i, e, id, boolean) {
+    // для переключения удалить-добавить важное
     e.target.parentNode.classList.remove('visible');
-    this.emailServ.httpPost('http://10.0.1.33:3000/mail/flagged', {id : id, flag: boolean}).subscribe();
-     this.emailServ.lettersList[i].flagged = ! this.emailServ.lettersList[i].flagged;
+    this.emailServ
+      .httpPost('http://10.0.1.33:3000/mail/flagged', { id: id, flag: boolean })
+      .subscribe();
+    this.emailServ.lettersList[i].flagged = !this.emailServ.lettersList[i]
+      .flagged;
   }
 
-// tslint:disable-next-line:max-line-length
-sent_incomingChecker(i) {  // проверка на входящие - исходящие сообщения, ибо исходящие не имеют статуса спама и важных!!!! (без нее глючат исходящие)
+  // tslint:disable-next-line:max-line-length
+  sent_incomingChecker(i) {
+    // проверка на входящие - исходящие сообщения, ибо исходящие не имеют статуса спама и важных!!!! (без нее глючат исходящие)
     if (this.emailServ.typeMess !== 'sent') {
       return true;
-    } else { return false; }
-}
+    } else {
+      return false;
+    }
+  }
 
+  scrollDown() {
+    const container = document.querySelector('.letter__container');
+    const maxScrollHeight = container.scrollHeight; //     **высота скрытого блока   height
+    const maxHeight = container.getBoundingClientRect().height; // **высота видимой области
+    const scrollPosition = container.scrollTop; // ** величина текущей прокрутки scroll
+    const maxScroll = maxScrollHeight - maxHeight; //  100% от макс возможного скролла
+    const persent = (scrollPosition * 100) / maxScroll; // текущий скролл в процентах
 
-scrollDown() {
-  const container = document.querySelector('.letter__container');
-  const maxScrollHeight = container.scrollHeight;  //     **высота скрытого блока   height
-  const maxHeight = container.getBoundingClientRect().height;   // **высота видимой области
-  const scrollPosition = container.scrollTop; // ** величина текущей прокрутки scroll
-  const maxScroll = maxScrollHeight - maxHeight;  //  100% от макс возможного скролла
-  const persent = (scrollPosition * 100) / maxScroll;  // текущий скролл в процентах
-
-  if (persent > 85) {
-        if (this.emailServ.stopFlag === false) {
-              if (this.emailServ.dataLetters !== this.emailServ.lettersAmount) {
-                this.counterAmount = 0;
-                return;
-              }
-              this.emailServ.stopFlag = true;
-              this.counterAmount = this.counterAmount + this.emailServ.lettersAmount;
-            this.emailServ.httpPost(
-              this.emailServ.adress,
-              // tslint:disable-next-line:max-line-length
-              {address: this.emailServ.idPostForHTTP, box: `${this.emailServ.selectNum}`, limit: `${this.emailServ.lettersAmount}`, offset: `${this.counterAmount}`}).subscribe((data) => {
-                this.emailServ.lettersList = this.emailServ.lettersList.concat(data);
-                this.emailServ.stopFlag = false;
-                this.emailServ.dataLetters = data.length;
-                this.emailServ.stateServ();
-                } );
+    if (persent > 85) {
+      if (this.emailServ.stopFlag === false) {
+        if (this.emailServ.dataLetters !== this.emailServ.lettersAmount) {
+          // this.counterAmount = 0;
+          return;
+        }
+        this.emailServ.stopFlag = true;
+        this.counterAmount = this.counterAmount + this.emailServ.lettersAmount;
+        this.emailServ
+          .httpPost(
+            this.emailServ.adress,
+            // tslint:disable-next-line:max-line-length
+            {
+              address: this.emailServ.idPostForHTTP,
+              box: `${this.emailServ.selectNum}`,
+              limit: `${this.emailServ.lettersAmount}`,
+              offset: `${this.counterAmount}`
+            }
+          )
+          .subscribe(data => {
+            this.emailServ.lettersList = this.emailServ.lettersList.concat(
+              data
+            );
+            this.emailServ.stopFlag = false;
+            this.emailServ.dataLetters = data.length;
+            this.emailServ.stateServ();
+          });
       }
     }
-}
+  }
 
-
-deleteRestoreLetter(id, e, box) {
-
-  e.target.parentNode.classList.remove('visible');
-  e.target.closest('.letter__prev').classList.add('dellLetter');
-  setTimeout(() => {
-    const idelem = this.emailServ.selectedLetter;
-    this.emailServ.httpPost('http://10.0.1.33:3000/mail/setbox', {id : id, box: `${box}`}).subscribe();
+  deleteRestoreLetter(id, e, box) {
+    e.target.parentNode.classList.remove('visible');
+    e.target.closest('.letter__prev').classList.add('dellLetter');
+    setTimeout(() => {
+      const idelem = this.emailServ.selectedLetter;
+      this.emailServ
+        .httpPost('http://10.0.1.33:3000/mail/setbox', {
+          id: id,
+          box: `${box}`
+        })
+        .subscribe();
       for (let i = 0; i < this.emailServ.lettersList.length; i++) {
         if (this.emailServ.lettersList[i].id === idelem.id) {
           this.emailServ.selectedLetter = this.emailServ.lettersList[i];
           this.emailServ.index = i;
         }
-    }
+      }
 
-  this.emailServ.lettersList = this.emailServ.lettersList.filter((val , ind) => {
-    if (val.id !== id) {
-      return val;
-    }
-    });
-    if (this.emailServ.lettersList.length === 0) {
-      this.rout.navigate([this.emailServ.urlParams]);
-    }
-    }, 500);
-}
-
-
-
-deleteRestoreLettersAll(box) {
-  const id_for_delete = this.emailServ.idLetters;
-  this.emailServ.lettersList.filter((val , ind, arr) => {
-    for (const key of id_for_delete) {
-      if (val.id === key) {
-        this.emailServ.httpPost('http://10.0.1.33:3000/mail/setbox', {id : `${val.id}`, box: `${box}`}).subscribe();
-        arr[ind] = 'null';
-            }
+      this.emailServ.lettersList = this.emailServ.lettersList.filter(
+        (val, ind) => {
+          if (val.id !== id) {
+            return val;
+          }
         }
-      });
-      this.emailServ.lettersList = this.emailServ.lettersList.filter(a => a !== 'null');
-      this.counterFroDownload = this.counterFroDownload + this.emailServ.idLetters.length;
-      setTimeout(() => {
-        this.emailServ.httpPost(
-          this.emailServ.adress,
-          // tslint:disable-next-line:max-line-length
-          {address: this.emailServ.idPostForHTTP, box: `${this.emailServ.selectNum}`, limit: `${this.emailServ.idLetters.length}`, offset: `${this.counterFroDownload}`}).subscribe((data) => {
-            console.log(data);
-            this.emailServ.lettersList = this.emailServ.lettersList.concat(data);
-            this.emailServ.lettersList = this.emailServ.lettersList.filter((val, ind, self) => {
-              return self.indexOf(val) === ind; } ); // фильтрую дублирование
-            this.emailServ.stateServ();
-    });
-    this.emailServ.hideAvatars = [];
-      this.emailServ.idLetters = [];
-      this.emailServ.checkerTrash();
-      this.counterFroDownload = 0;
-      }, 1000);
-}
-}
+      );
+      if (this.emailServ.lettersList.length === 0) {
+        this.rout.navigate([this.emailServ.urlParams]);
+      }
+    }, 500);
+  }
 
+  deleteRestoreLettersAll(box) {
+    const id_for_delete = this.emailServ.idLetters;
+    this.emailServ.lettersList = this.emailServ.lettersList.filter((val, ind, arr) => {
+      for (const key of id_for_delete) {
+        if (val.id === key) {
+          this.emailServ
+            .httpPost('http://10.0.1.33:3000/mail/setbox', {
+              id: `${val.id}`,
+              box: `${box}`
+            })
+            .subscribe();
+          arr[ind] = 'null';
+        }
+      }
+    });
+    this.emailServ.lettersList = this.emailServ.lettersList.filter(
+      a => a !== 'null'
+    );
+    if (this.emailServ.lettersList.length <= 15) {  // если подзагруза не было, восстанавливаю стартовое кол-во писем
+      this.emailServ
+      .httpPost(
+        this.emailServ.adress,
+        // tslint:disable-next-line:max-line-length
+        {
+          address: this.emailServ.idPostForHTTP,
+          box: `${this.emailServ.selectNum}`,
+          limit: `${this.emailServ.lettersAmount}`,
+          offset: `0`
+        }
+      )
+      .subscribe(data => {
+        this.emailServ.lettersList = data;
+        this.emailServ.stateServ();
+      });
+    }
+              this.emailServ.stateServ(); // save state on service
+              this.emailServ.hideAvatars = []; // чтоб инпуты работали
+              this.emailServ.idLetters = []; // обнуляю корзину на удаление
+              this.emailServ.checkerTrash(); // убираю иконку
+  }
+}
