af2857ced2ab9b23317ae8adcff2369f1fa6528e
Vasilkov Dmitry <riset24@mail.ru>
2018-11-20 18:29:18 +0300

    attachment

--- src/app/email/email-view/attach.ts
+++ src/app/email/email-view/attach.ts
@@ -1,6 +1,8 @@
 export const attachers = [
+    {title: 'Акт приемки от ООО "Строй-Монтаж" по договору № А-711-02-11 от 02.22.2018 подписанный С.О', size: '7kb', type: 'doc'},
     {title: 'sdasdsada.xls', size: '12kb', type: 'xls'}, {title: 'ada.pdf', size: '22kb', type: 'pdf'},
-    {title: 'lalaalla.jpg', size: '7kb', type: 'jpg'}, {title: 'text.word', size: '7kb', type: 'doc'},
+    {title: 'lalaalla.jpg', size: '7kb', type: 'jpg'}, {title: 'Договор №11 от 7.12.2016г.word', size: '7kb', type: 'doc'},
     {title: 'text.txt', size: '7kb', type: 'txt'}, {title: 'imagepng.png', size: '700kb', type: 'png'},
     {title: 'music.mp3', size: '3900kb', type: 'mp3'},
+    {title: 'Приложение к договору подряда №774 от 18 июля 2017 года с исправленными датами на подпись', size: '7000000kb', type: 'doc'}
 ];
