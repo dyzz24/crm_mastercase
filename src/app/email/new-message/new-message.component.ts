9a3a1960e2858e31025642a1fc0659d752507c47
Vasilkov Dmitry <riset24@mail.ru>
2018-11-28 18:34:37 +0300

    полный ответ без функционала баблов

--- src/app/email/new-message/new-message.component.ts
+++ src/app/email/new-message/new-message.component.ts
@@ -3,7 +3,6 @@ import { EmailServiceService } from '../email-service.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { SocketService } from '../../socket.service';
 import { AuthorizationService } from '../../authorization.service';
 
 
@@ -13,19 +12,24 @@ import { AuthorizationService } from '../../authorization.service';
   styleUrls: ['./new-message.component.scss']
 })
 export class NewMessageComponent implements OnInit, DoCheck {
-  constructor(public emailServ: EmailServiceService, private _rout: Router,
+  constructor(
+    @Inject(EmailServiceService) public emailServ: EmailServiceService,
+    private _rout: Router,
     private http: HttpClient,
     @Inject(AuthorizationService) private authorizationServ: AuthorizationService) { }
 
 private from;
-private to = this.emailServ.to_answer;
+private to = [this.emailServ.to_answer];
 private copy = this.emailServ.to_all_answer;
+private hidden_copy = [];
 private subject = this.emailServ.to_subject;
 private messages;
+
   ngOnInit() {
+    // console.log(this.emailServ.to_all_answer.length)
   }
   ngDoCheck() {
-    // console.log(this.to);
+    // console.log(this.emailServ.to_all_answer);
   }
 
   public httpPost(url: string, body, options?): Observable<any> {
@@ -44,7 +48,7 @@ private messages;
     this.httpPost(
       `${this.emailServ.ip}/mail/send`,
       // tslint:disable-next-line:max-line-length
-      { subject: this.subject, text: this.messages, html: this.messages, to: this.to.split(', ')}).subscribe((data) => {
+      { subject: this.subject, text: this.messages, html: this.messages, to: this.to}).subscribe((data) => {
   });
 
 }}
