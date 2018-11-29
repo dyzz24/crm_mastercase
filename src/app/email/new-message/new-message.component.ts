079923210b79db1077410dbeb84c2a42b88fb95a
Vasilkov Dmitry <riset24@mail.ru>
2018-11-29 18:06:21 +0300

    bables creates / add names in array

--- src/app/email/new-message/new-message.component.ts
+++ src/app/email/new-message/new-message.component.ts
@@ -19,25 +19,45 @@ export class NewMessageComponent implements OnInit, DoCheck {
     @Inject(AuthorizationService) private authorizationServ: AuthorizationService) { }
 
 private from;
-private to = [this.emailServ.to_answer];
-private to_text;
-private copy = this.emailServ.to_all_answer;
-private hidden_copy = [];
-private subject = this.emailServ.to_subject;
+private to = [this.emailServ.to_answer]; // array for send
+private copy = [this.emailServ.to_all_answer]; // array for send
+private hidden_copy = []; // array for send
+private subject = this.emailServ.to_subject; // тема для отправки
+
 private messages;
 
   ngOnInit() {
-    // console.log(this.emailServ.to_all_answer.length)
+    // console.log(this.copy)
   }
   ngDoCheck() {
-    console.log(this.to_text);
+    // console.log(this.to);
   }
 
   public httpPost(url: string, body, options?): Observable<any> {
     return this.http.post(url, body, {headers: {Authorization: `Bearer ${this.authorizationServ.accessToken}`}});
   }
 
-  
+  add_data(arr, e) { // срабатывает по блюру, функция принимает массив для работы
+      if (e.target.value === undefined || e.target.value === '') { // если пустая строка - выхожу
+        return;
+      }
+      if (arr[0] === '') { // если первый элемент  = ""
+                          // (такое бывает, когда нажимаю создать новое письмо, ибо передает '' от сервиса), удаляю его
+          arr.shift();
+      }
+      arr.push(e.target.value); // добавляю в массив значение с таргета
+      e.target.value = ''; // чищу значение с таргета
+  }
+  add_data_keyEvent(arr,  e) { // срабатывает при клике на Enter, функция принимает массив для работы
+    if (e.key === 'Enter' && e.target.value !== '') { // отлавливаю клик на Enter
+      if (arr[0] === '') { // аналогично действию в блюре
+        arr.shift();
+      }
+      arr.push(e.target.value);
+      e.target.value = '';
+    }
+  }
+
 
 
   closeViewer() {
@@ -48,10 +68,10 @@ private messages;
   }
 
   sendMessage() {
-  //   this.httpPost(
-  //     `${this.emailServ.ip}/mail/send`,
-  //     // tslint:disable-next-line:max-line-length
-  //     { subject: this.subject, text: this.messages, html: this.messages, to: this.to}).subscribe((data) => {
-  // });
+    this.httpPost(
+      `${this.emailServ.ip}/mail/send`,
+      // tslint:disable-next-line:max-line-length
+      { subject: this.subject, text: this.messages, html: this.messages, to: this.to}).subscribe((data) => {
+  });
 
 }}
