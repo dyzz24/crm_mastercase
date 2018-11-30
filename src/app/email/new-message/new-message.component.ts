6b11e9609a176ced6d0956403059246f95472769
Vasilkov Dmitry <riset24@mail.ru>
2018-11-30 14:43:30 +0300

    bubls + bubls кто в работе + bubls в новых сообщениях

--- src/app/email/new-message/new-message.component.ts
+++ src/app/email/new-message/new-message.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthorizationService } from '../../authorization.service';
+import { ToastrService} from 'ngx-toastr';
 
 
 @Component({
@@ -16,6 +17,7 @@ export class NewMessageComponent implements OnInit, DoCheck {
     @Inject(EmailServiceService) public emailServ: EmailServiceService,
     private _rout: Router,
     private http: HttpClient,
+    @Inject(ToastrService) private toastrServ: ToastrService,
     @Inject(AuthorizationService) private authorizationServ: AuthorizationService) { }
 
 private from;
@@ -25,6 +27,7 @@ private hidden_copy = []; // array for send
 private subject = this.emailServ.to_subject; // тема для отправки
 
 private messages;
+private messages_sending = false;
 
   ngOnInit() {
     console.log(typeof(this.copy));
@@ -48,17 +51,50 @@ private messages;
                           // (такое бывает, когда нажимаю создать новое письмо, ибо передает '' от сервиса), удаляю его
           arr.shift();
       }
+      const regExsp = /[а-яё]/i; // регулярка для проверки русских символов
+      if (e.target.value.indexOf('@') < 0 || e.target.value.search(regExsp) >= 0) { // если в троке есть @ или русский символ
+        this.showError('Введите корректный адрес (en + @)'); // выдаст сообщение
+        return;
+      }
       arr.push(e.target.value); // добавляю в массив значение с таргета
+      arr.filter((val, ind, self) => {
+        if (self.indexOf(val) !== ind) { // если такое значение уже есть в массиве
+          arr.pop(); // удаляю последнее добавленное push'ем значение
+          this.showError('Адрес уже есть');
+        }});
       e.target.value = ''; // чищу значение с таргета
   }
-  add_data_keyEvent(arr,  e) { // срабатывает при клике на Enter, функция принимает массив для работы
+  add_data_keyEvent(arr,  e) { // срабатывает при клике на Enter, функция принимает массив для работы, действие аналогично ф-ии сверху
     if (e.key === 'Enter' && e.target.value !== '') { // отлавливаю клик на Enter
-      if (arr[0] === '') { // аналогично действию в блюре
+      if (arr[0] === '') {
         arr.shift();
       }
+      const regExsp = /[а-яё]/i;
+      if (e.target.value.indexOf('@') < 0 || e.target.value.search(regExsp) >= 0) {
+        this.showError('Введите корректный адрес (en + @)');
+        return;
+      }
       arr.push(e.target.value);
+      arr.filter((val, ind, self) => {
+        if (self.indexOf(val) !== ind) {
+          arr.pop();
+          this.showError('Адрес уже есть');
+        }});
       e.target.value = '';
     }
+  }
+
+  delete_data_keyEvent(arr, e) { // удаление при клике на Backspace
+    if (e.key === 'Backspace') {
+      arr.pop();
+    }
+  }
+  delete_data_clickEvent(arr, e, index) { // удаление при клике на крестик в бабле
+    arr.splice(index, 1); // удалили по текущему индексу
+  }
+
+  showError(param) {
+    this.toastrServ.error(param);
   }
 
 
