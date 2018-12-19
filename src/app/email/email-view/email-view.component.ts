1085bd83ac62d8c586a4b385694c9e0de4324131
Vasilkov Dmitry <riset24@mail.ru>
2018-12-19 13:30:39 +0300

    txt ok

--- src/app/email/email-view/email-view.component.ts
+++ src/app/email/email-view/email-view.component.ts
@@ -133,6 +133,7 @@ export class EmailViewComponent implements OnInit, DoCheck, OnDestroy {
 
         this.emailServ.haveResponse = true;
         this.selectedLetter = Object.assign(part_one_data, dataMails);
+        console.log(this.selectedLetter);
         this.checkerLengthArray_bcc_cc();
         this.checkerLength_addressess();
 
